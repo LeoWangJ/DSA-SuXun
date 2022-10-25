a27de401f50b1676a8af6d08d7ad4bc3bf1d2183
leowang <rfv7855659@gmail.com>
2022-10-25 13:21:16 +0800

    ✨ 110

---
+++ tree/110.balanced-binary-tree.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * a binary tree in which the left and right subtrees of every node differ in height by no more than 1
+ * 
+ * need to find left deepest node & right deepest node
+ * and how to find?
+ *  
+ *        5 
+ *    2       8
+ *  1   3   6   7  
+ * 
+ * it's difficult for me 
+ */
+var isBalanced = function (root) {
+    if (root === null) return true
+    return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+    function depth(root) {
+        if (root === null) return 0
+        return Math.max(depth(root.left), depth(root.right)) + 1
+    }
+};
+
+/**
+ * bottom to up
+ * https://leetcode.cn/problems/balanced-binary-tree/solution/shu-ju-jie-gou-he-suan-fa-ping-heng-er-c-ckkm/
+ */
+var isBalanced = function (root) {
+    const UNBALANCE = -1
+    return recursion(root) != UNBALANCE
+
+    function recursion(root) {
+        if (root === null) return 0
+        // 如果左子節點不是平衡二叉樹，直接返回UNBALANCED
+        let left = recursion(root.left)
+        if (left === UNBALANCE) return UNBALANCE
+        // 如果右子節點不是平衡二叉樹，直接返回UNBALANCED
+        let right = recursion(root.right)
+        if (right === UNBALANCE) return UNBALANCE
+        // 如果左右子節點都是平衡二叉樹，但他們的高度相差大於1，直接返回UNBALANCED，否則就返回二叉樹中節點的最大高度
+        return Math.abs(left - right) > 1 ? UNBALANCE : Math.max(left, right) + 1
+    }
+};
--- tree/226.invert-binary-tree.js
+++ tree/226.invert-binary-tree.js
@@ -37,16 +37,75 @@ var invertTree = function (root) {
     return root
 };
 
+/**
+ * https://leetcode.cn/problems/invert-binary-tree/solution/dong-hua-yan-shi-liang-chong-shi-xian-226-fan-zhua/
+ * 
+ * invert tree
+ * 
+ *       5 
+ *    2  -> 8
+ *  1  3  6   7
+ * 
+ * queue = [5]
+ * 
+ *       5 
+ *    8     2
+ *  6->7  1   3
+ * 
+ * queue= [8,2]
+ * queue= [2]
+ * 
+ *       5 
+ *    8     2
+ *  7  6  1 ->3
+ * 
+ * queue= []
+ * 
+ *       5 
+ *    8     2
+ *  7  6  3   1
+ * 
+ * time complexity O(n)
+ * extra space complexity O(n)
+ * 
+ * 廣度遍歷
+ */
 var invertTree = function (root) {
-    function invert(root) {
-        if (root === null) return null
-        let tempLeft = root.left
-        root.left = root.right
-        root.right = tempLeft
-        invert(root.left)
-        invert(root.right)
-    }
+    if (root === null) return null
+    let queue = [root]
+    while (queue.length !== 0) {
+        let temp = queue.shift()
+        let left = temp.left
+        temp.left = temp.right
+        temp.right = left
+        if (temp.left !== null) {
+            queue.push(temp.left)
+        }
 
-    invert(root)
+        if (temp.right !== null) {
+            queue.push(temp.right)
+        }
+    }
+    return root
+};
+
+/**
+ * iteratively 
+ */
+var invertTree = function (root) {
+    let stack = []
+    let cur = root
+    while (cur || stack.length !== 0) {
+        if (cur) {
+            let left = cur.left
+            cur.left = cur.right
+            cur.right = left
+            stack.push(cur)
+            cur = cur.left
+        } else {
+            cur = stack.pop()
+            cur = cur.right
+        }
+    }
     return root
 };
